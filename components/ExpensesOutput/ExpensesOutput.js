26ed9d8cffc981b5706b0887e3094c50d3234027
Joshua Carroll <carrolljosh1976@gmail.com>
2024-04-13 15:59:21 -0400

    added some dummy data to ExpensesOutput component

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,10 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpenseList from "./ExpenseList";
 
-export default function ExpensesOutput({expenses}) {
+export default function ExpensesOutput({expenses,expensesPeriod}) {
     return (
         <View>
-            <ExpensesSummary />
+            <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
             <ExpenseList />
         </View>
     )
