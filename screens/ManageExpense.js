f69a49b10f14839cf3f9de3e0c7d14b24ab0d227
Joshua Carroll <carrolljosh1976@gmail.com>
2024-05-26 15:22:29 -0400

    added button for deleting an expense inside EditExpense, along with background color and border top

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 export default function ManageExpense({route, navigation}) {
     const editedExpenseId = route.params?.expenseId;
@@ -11,7 +13,32 @@ export default function ManageExpense({route, navigation}) {
         })
     },[navigation,isEditing]);
     
+    function deleteExpenseHandler() {
+
+    }
+
     return (
-        <Text>ManageExpense Screen</Text>
+        <View style={styles.container}>
+            {isEditing && (
+                <View style={styles.deleteContainer}>
+                    <IconButton icon="trash" color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler} />
+                </View>
+            )}
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: "center"
+    }
+})
